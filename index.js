f46459287df6e4c233229253b980ca832b61372c
MrKireko <mrkireko@gmail.com>
2015-07-14 20:16:39 +0200

    better everything

--- index.js
+++ index.js
@@ -7,11 +7,31 @@ $(document).ready(function() {
 			$("#ttl").css("margin-left", "1vh");
 			$("#sidenav").css("margin-left", "-26%");
 			navopen = false;
+			setTimeout(function(){
+				$("#list").css("opacity", "0");
+			}, 300);
 		} else {
 			$("#ttl").css("margin-left", "19%");
 			$("#sidenav").css("margin-left", "0px");
 			navopen = true;
+			setTimeout(function(){
+				$("#list").css("opacity", "1");
+			}, 300);
 		}
+	});
+	
+	$(".mi").click(function(){
+		$("#ttl").css("margin-left", "1vh");
+		$("#sidenav").css("margin-left", "-26%");
+		navopen = false;
+		setTimeout(function(){
+			$("#openbutton").css("margin-top", "-16%")
+			$("#ttl").css("margin-top", "-16%")
+		}, 300);
+		setTimeout(function(){
+			$("#appbar").css("margin-top", "-16%")
+		}, 650);
+//		$("body").css("opacity", "0");
 	});
 	
 });
